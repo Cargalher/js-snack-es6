3f54de774e5ece1c8019d40388dd000a3c890433
Cargalher <cargalher@gmail.com>
2021-05-11 15:36:22 +0200

    first snack solved

--- main.js
+++ main.js
@@ -1,50 +1,59 @@
 /*
 Snack 1:
 Creare un array di oggetti, ogni oggetto descriverà una bici da corsa con le seguenti proprietà:
-nome e
-peso.
+- nome e
+- peso.
 Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
 */
 
-var biciCorsa = [
+let biciCorsa = [
     
-   bici1 = {
+    {
        nome: "Cimino",
        peso: 9.0,
     },
-    bici2 = {
+
+    {
         nome: "Bianchi",
         peso: 5.4,
     },
-    bici3 = {
+
+    {
         nome: "Olmo",
         peso: 7.0,
     },
-    bici4 = {
+
+    {
         nome: "Orbea",
         peso: 4.5,
     },
-    bici5 = {
+
+    {
         nome: "Cannondale",
         peso: 8.0,
     },
-    bici6 = {
+
+    {
         nome: "Giant",
         peso: 5.7,
     },
-    bici7= {
+
+    {
         nome: "Trek",
         peso: 4.6,
     },
-    bici8 = {
+
+    {
         nome: "Scott",
         peso: 6.2,
     },
-    bici9= {
+
+    {
         nome: "Canyon",
         peso: 8.4,
     },
-    bici10 = {
+
+    {
         nome: "BMC",
         peso: 7.7,
     },
@@ -52,7 +61,10 @@ var biciCorsa = [
 ]
  
 
-
+let lightest = biciCorsa.reduce(function(lightest, bici){
+    return bici.peso<lightest.peso ? bici : lightest;
+},biciCorsa[0]);
+console.log(lightest);
 /*
 Snack2:
 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà:
